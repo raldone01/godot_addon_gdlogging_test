163e5d4334c67b077a03edaba8bd519718388b20
raldone01 <raldone01@gmail.com>
2024-07-08 02:00:26 +0200

    Improve the test coverage.

--- main.gd
+++ main.gd
@@ -5,7 +5,10 @@ func _ready() -> void:
 	var console_sink := Log.ConsoleSink.new()
 	Log.add_sink(console_sink)
 	Log.info("Hello, World!")
-	pass # Replace with function body.
+	Log.warning("This is a warning.")
+	Log.error("This is an error.")
+	Log.debug("This is a debug message.")
+	Log.trace("This is a trace message.")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
